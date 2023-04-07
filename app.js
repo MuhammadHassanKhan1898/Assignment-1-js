c0feaf8b5383a32b80fef64b2f4e113bb471fc23
MuhammadHassanKhan1898 <muhammadhassankhan1898@gmail.com>
2023-04-07 19:44:11 +0500

    Javascript crash course Assignment 1

---
+++ app.js
@@ -0,0 +1,112 @@
+// *****************Task 1*****************************
+
+// let num1 = +prompt("Enter First Number");
+// let num2 = +prompt("Enter second Number");
+
+// if( num1 > num2 ){
+//     console.log(num1 + " is a larger number");
+// }
+// else{
+//     console.log(num2 + " is a larger number.");
+// }
+
+// *****************Task 2*****************************
+
+// let num = +prompt("Enter a Number");
+
+// if( num >= 0 ){
+//     console.log("The sign is +.");
+// }
+// else{
+//     console.log("The sign is -.");
+// }
+
+// *****************Task 3*****************************
+
+// let num1 = +prompt("Enter First Number");
+// let num2 = +prompt("Enter Second Number");
+// let num3 = +prompt("Enter Third Number");
+// let num4 = +prompt("Enter Fourth Number");
+// let num5 = +prompt("Enter Fifth Number");
+// let largest_nmber;
+// if(num1 >= num2 && num1 >= num3 && num1 >= num4 && num1 >= num5){
+//     largest_nmber = num1;
+// }
+// else if (num2 >= num1 && num2 >= num3 && num2 >= num4 && num2 >= num5){
+//     largest_nmber = num2;
+// }
+// else if (num3 >= num1 && num3 >= num2 && num3 >= num4 && num3 >= num5){
+//     largest_nmber = num3;
+// }
+// else if (num4 >= num1 && num4 >= num2 && num4 >= num3 && num4 >= num5){
+//     largest_nmber = num4;
+// }
+// else if (num5 >= num1 && num5 >= num2 && num5 >= num3 && num5 >= num4){
+//     largest_nmber = num5;
+// }
+
+
+// console.log("The largest number is: " + largest_nmber);
+
+// *****************Task 4*****************************
+
+// for (var i = 0; i <= 15; i++)
+// {
+//     if (i === 0){
+//         console.log(i + " is a Even number: ");
+//     }
+//     else if (i % 2 === 0){
+//         console.log(i + " is a odd number: ");
+// }
+//     else{
+//         console.log(i + " is a odd number: ");
+//     }
+// }
+
+// *****************Task 5*****************************
+
+// let marks = +prompt("Enter your number");
+    
+//     if (marks < 60) {
+//         console.log("Your Grade: F");
+//     }
+//     else if (marks < 70) {
+//         console.log("Your Grade: D");
+//     }
+//     else if (marks < 80) {
+//         console.log("Your Grade: C");
+//     } 
+//     else if (marks< 90) {
+//         console.log("Your Grade: B");
+//     } 
+//     else if (marks < 100) {
+//         console.log("Your Grade: A");
+//     }
+
+
+// *****************Task 6*****************************
+
+// for (var j = 1; j <= 100; j++) 
+// {
+//     if (j % 3 === 0 && j % 5 === 0) {
+//         console.log(j + " FizzBuzz");
+//     }
+//     else if (j % 3 === 0) {
+//         console.log(j + " Fizz");
+//     }
+//     else {
+//         console.log(j + " Buzz");
+//     }
+// }
+
+
+// *****************Task 7*****************************
+
+// for (var outerLoop = 1; outerLoop <= 5; outerLoop++)
+//     {
+//         for (var innerLoop = 1; innerLoop <= outerLoop; innerLoop++ )
+//         {
+//            document.write("*");
+//         }
+//         document.write("<br />");
+//     }
